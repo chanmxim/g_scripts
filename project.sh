51b79d86e2d892c65e9ae31e9143431662388236
chanmxim <chanmxim@gmail.com>
2026-09-18 13:11:35 -0400

    add readonly to variables in tmux project setup script

--- project.sh
+++ project.sh
@@ -1,6 +1,7 @@
-CURRENT_LOCATION="${PWD}"
-FOLDER_NAME="$(basename "${CURRENT_LOCATION}")"
-SESSION_NAME="project_${FOLDER_NAME}"
+readonly CURRENT_LOCATION="${PWD}"
+readonly FOLDER_NAME="$(basename "${CURRENT_LOCATION}")"
+readonly SESSION_NAME="project_${FOLDER_NAME}"
+readonly LEFT_PANE_WIDTH="40"
 
 echo "Starting tmux session: ${SESSION_NAME}"
 
@@ -8,13 +9,13 @@ if tmux has-session -t "${SESSION_NAME}" 2>/dev/null; then
   echo "Session ${SESSION_NAME} already exists. Attaching to the existing session..."
 else
   # Get width and height of the current terminal window
-  TERM_COLS="$(tput cols)"
-  TERM_LINES="$(tput lines)"
+  readonly TERM_COLS="$(tput cols)"
+  readonly TERM_LINES="$(tput lines)"
 
   tmux new-session -d -x "${TERM_COLS}" -y "${TERM_LINES}" -s "${SESSION_NAME}" -c "${CURRENT_LOCATION}"
 
   # Create left pane 40 column wide
-  tmux split-window -h -b -l 40 -t "${SESSION_NAME}" -c "${CURRENT_LOCATION}"
+  tmux split-window -h -b -l "$LEFT_PANE_WIDTH" -t "${SESSION_NAME}" -c "${CURRENT_LOCATION}"
 fi
 
 tmux attach-session -t "${SESSION_NAME}"
